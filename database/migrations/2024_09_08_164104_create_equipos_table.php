1bf3a544a5ea1360262ccc52e0a3592821ae0836
Michael Asencio <164639782+Michael383883@users.noreply.github.com>
2024-09-08 18:32:10 -0400

    Fix equipos table migration to align with updated schema

--- database/migrations/2024_09_08_164104_create_equipos_table.php
+++ database/migrations/2024_09_08_164104_create_equipos_table.php
@@ -14,6 +14,7 @@ class CreateEquiposTable extends Migration
     public function up()
     {
         Schema::create('equipos', function (Blueprint $table) {
+            $table->timestamps();
             $table->id();
             $table->timestamps();
         });
