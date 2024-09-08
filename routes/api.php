1c42e3fa1010c587dd52a6dbf3181194ebe71266
Michael Asencio <164639782+Michael383883@users.noreply.github.com>
2024-09-08 18:32:11 -0400

    Update API routes to include new endpoints for Grupo management

--- routes/api.php
+++ routes/api.php
@@ -3,19 +3,46 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+use App\Http\Controllers\ActorController;
+use App\Http\Controllers\DocenteController;
+use App\Http\Controllers\EstudianteController;
+use App\Http\Controllers\GrupoController;
+
+
+
+// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 //prueva
-Route::get('/get-all-users', 'UserController@getAllUsers');
+// Rutas para ActorController
+Route::get('/actores', [ActorController::class, 'index']);
+Route::get('/actores/{id}', [ActorController::class, 'show']);
+Route::post('/actores', [ActorController::class, 'store']);
+Route::put('/actores/{id}', [ActorController::class, 'update']);
+Route::patch('/actores/{id}', [ActorController::class, 'updatePartial']);
+Route::delete('/actores/{id}', [ActorController::class, 'delete']);
+
+// // Rutas para DocenteController
+// Route::get('/docentes', [DocenteController::class, 'index']);
+// Route::get('/docentes/{id}', [DocenteController::class, 'show']);
+// Route::post('/docentes', [DocenteController::class, 'store']);
+// Route::put('/docentes/{id}', [DocenteController::class, 'update']);
+// Route::patch('/docentes/{id}', [DocenteController::class, 'updatePartial']);
+// Route::delete('/docentes/{id}', [DocenteController::class, 'delete']);
+
+// // Rutas para EstudianteController
+// Route::get('/estudiantes', [EstudianteController::class, 'index']);
+// Route::get('/estudiantes/{id}', [EstudianteController::class, 'show']);
+// Route::post('/estudiantes', [EstudianteController::class, 'store']);
+// Route::put('/estudiantes/{id}', [EstudianteController::class, 'update']);
+// Route::patch('/estudiantes/{id}', [EstudianteController::class, 'updatePartial']);
+// Route::delete('/estudiantes/{id}', [EstudianteController::class, 'delete']);
+
+// // Rutas para GrupoController
+// Route::get('/grupos', [GrupoController::class, 'index']);
+// Route::get('/grupos/{id}', [GrupoController::class, 'show']);
+// Route::post('/grupos', [GrupoController::class, 'store']);
+// Route::put('/grupos/{id}', [GrupoController::class, 'update']);
+// Route::patch('/grupos/{id}', [GrupoController::class, 'updatePartial']);
+// Route::delete('/grupos/{id}', [GrupoController::class, 'delete']);
