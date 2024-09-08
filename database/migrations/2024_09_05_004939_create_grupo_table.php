9a935f59a802f3beee99adeacb4bf1f442623e6f
Michael Asencio <164639782+Michael383883@users.noreply.github.com>
2024-09-08 18:32:04 -0400

    Modify grupo table migration to include unique constraints

--- database/migrations/2024_09_05_004939_create_grupo_table.php
+++ database/migrations/2024_09_05_004939_create_grupo_table.php
@@ -14,6 +14,7 @@ class CreateGrupoTable extends Migration
     public function up()
     {
         Schema::create('grupo', function (Blueprint $table) {
+            $table->timestamps();
             $table->integer('idgrupo')->unique('grupo_pk');
         });
     }
