ed3f08c88c6d5e7734e77d8a2253eb633f7292eb
julio <juliocesarsevericheorellana@gmail.com>
2024-10-16 08:20:58 -0400

    Update Tasks

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Api\RolesController;
 use App\Http\Controllers\Api\SiscodeController;
 use App\Http\Controllers\Api\SprintsController;
 use App\Http\Controllers\Api\StoriesController;
+use App\Http\Controllers\Api\TasksController;
 use App\Http\Controllers\Api\TeamsController;
 use App\Http\Controllers\Api\TrackingsController;
 use App\Http\Controllers\Api\UsersController;
@@ -33,4 +34,5 @@ Route::group(["middleware" => ["auth:sanctum"]], function () {
     Route::apiResource("trackings", TrackingsController::class);
     Route::apiResource("weeklies", WeekliesController::class);
     Route::apiResource("stories", StoriesController::class);
+    Route::apiResource("tasks", TasksController::class);
 });
