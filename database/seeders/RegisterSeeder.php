4d5dc2ed14dd15e0b2f6e1dcbb33211790bd574a
julio <juliocesarsevericheorellana@gmail.com>
2024-10-27 18:12:36 -0400

    Update Seed

--- database/seeders/RegisterSeeder.php
+++ database/seeders/RegisterSeeder.php
@@ -55,7 +55,7 @@ class RegisterSeeder extends Seeder
         ]);
 
         // Teacher 1 User
-        $teacher = User::create([
+        $teacher1 = User::create([
             'nameuser' => 'Corina User',
             'lastnameuser' => 'Teacher',
             'emailuser' => 'corina@teacher.com',
@@ -63,8 +63,12 @@ class RegisterSeeder extends Seeder
             'idpermission' => $permissions['corina'],
         ]);
         RoleUser::create([
-            'iduser' => $teacher->iduser,
+            'iduser' => $teacher1->iduser,
             'idrol' => 2,
+        ]);
+        // Actualizar la tabla permissions con iduser
+        DB::table('permissions')->where('idpermission', $permissions['corina'])->update([
+            'iduser' => $teacher1->iduser,
         ]);
 
         // Teacher 2 User
@@ -79,6 +83,10 @@ class RegisterSeeder extends Seeder
             'iduser' => $teacher2->iduser,
             'idrol' => 2,
         ]);
+        // Actualizar la tabla permissions con iduser
+        DB::table('permissions')->where('idpermission', $permissions['leticia'])->update([
+            'iduser' => $teacher2->iduser,
+        ]);
 
         // Teacher 3 User
         $teacher3 = User::create([
@@ -91,6 +99,10 @@ class RegisterSeeder extends Seeder
         RoleUser::create([
             'iduser' => $teacher3->iduser,
             'idrol' => 2,
+        ]);
+        // Actualizar la tabla permissions con iduser
+        DB::table('permissions')->where('idpermission', $permissions['david'])->update([
+            'iduser' => $teacher3->iduser,
         ]);
 
         // Teacher 4 User
@@ -105,107 +117,165 @@ class RegisterSeeder extends Seeder
             'iduser' => $teacher4->iduser,
             'idrol' => 2,
         ]);
+        // Actualizar la tabla permissions con iduser
+        DB::table('permissions')->where('idpermission', $permissions['patricia'])->update([
+            'iduser' => $teacher4->iduser,
+        ]);
 
         // Student 1 User------------------------------
-        $student = User::create([
+
+        // Obtener el siscode antes de crear el student
+        $siscode1 = DB::table('siscode')->where('siscode', '202400001')->first();
+
+        // Crear el student
+        $student1 = User::create([
             'nameuser' => 'Student 1 User',
             'lastnameuser' => 'Student',
             'emailuser' => 'student1@example.com',
             'passworduser' => bcrypt('password*'),
-            'idsiscode' => 1,
-            'use_iduser' => $teacher->iduser,
+            'idsiscode' => $siscode1->idsiscode,
+            'use_iduser' => $teacher1->iduser,
         ]);
+
+        // Asignar el rol al student
         RoleUser::create([
-            'iduser' => $student->iduser,
+            'iduser' => $student1->iduser,
             'idrol' => 3,
         ]);
-        // Asigna el código SIS al estudiante
-        $siscode = DB::table('siscode')->where('siscode', '202400001')->first();
-        DB::table('siscode')->where('siscode', '202400001')->update(['iduser' => $student->iduser]);
+
+        // Actualizar la tabla siscode con iduser
+        DB::table('siscode')->where('siscode', '202400001')->update([
+            'iduser' => $student1->iduser,
+        ]);
 
         // Student 2 User------------------------------
+
+        // Obtener el siscode antes de crear el student
+        $siscode2 = DB::table('siscode')->where('siscode', '202400002')->first();
+
+        // Crear el student
         $student2 = User::create([
             'nameuser' => 'Student 2 User',
             'lastnameuser' => 'Student',
             'emailuser' => 'student2@example.com',
             'passworduser' => bcrypt('password*'),
-            'idsiscode' => 2,
-            'use_iduser' => $teacher->iduser,
+            'idsiscode' => $siscode2->idsiscode,
+            'use_iduser' => $teacher1->iduser,
         ]);
+
+        // Asignar el rol al student
         RoleUser::create([
             'iduser' => $student2->iduser,
             'idrol' => 3,
         ]);
-        // Asigna el código SIS al estudiante
-        $siscode2 = DB::table('siscode')->where('siscode', '202400002')->first();
-        DB::table('siscode')->where('siscode', '202400002')->update(['iduser' => $student2->iduser]);
+
+        // Actualizar la tabla siscode con iduser
+        DB::table('siscode')->where('siscode', '202400002')->update([
+            'iduser' => $student2->iduser,
+        ]);
 
         // Student 3 User------------------------------
+        
+        // Obtener el siscode antes de crear el student
+        $siscode3 = DB::table('siscode')->where('siscode', '202400003')->first();
+
+        // Crear el student
         $student3 = User::create([
             'nameuser' => 'Student 3 User',
             'lastnameuser' => 'Student',
             'emailuser' => 'student3@example.com',
             'passworduser' => bcrypt('password*'),
-            'idsiscode' => 3,
-            'use_iduser' => $teacher->iduser,
+            'idsiscode' => $siscode3->idsiscode,
+            'use_iduser' => $teacher1->iduser,
         ]);
+
+        // Asignar el rol al student
         RoleUser::create([
             'iduser' => $student3->iduser,
             'idrol' => 3,
         ]);
-        // Asigna el código SIS al estudiante
-        $siscode3 = DB::table('siscode')->where('siscode', '202400003')->first();
-        DB::table('siscode')->where('siscode', '202400003')->update(['iduser' => $student3->iduser]);
+
+        // Actualizar la tabla siscode con iduser
+        DB::table('siscode')->where('siscode', '202400003')->update([
+            'iduser' => $student3->iduser,
+        ]);
 
         // Student 4 User------------------------------
+        
+        // Obtener el siscode antes de crear el student
+        $siscode4 = DB::table('siscode')->where('siscode', '202400004')->first();
+
+        // Crear el student
         $student4 = User::create([
             'nameuser' => 'Student 4 User',
             'lastnameuser' => 'Student',
             'emailuser' => 'student4@example.com',
             'passworduser' => bcrypt('password*'),
-            'idsiscode' => 4,
-            'use_iduser' => $teacher->iduser,
+            'idsiscode' => $siscode4->idsiscode,
+            'use_iduser' => $teacher1->iduser,
         ]);
+
+        // Asignar el rol al student
         RoleUser::create([
             'iduser' => $student4->iduser,
             'idrol' => 3,
         ]);
-        // Asigna el código SIS al estudiante
-        $siscode4 = DB::table('siscode')->where('siscode', '202400004')->first();
-        DB::table('siscode')->where('siscode', '202400004')->update(['iduser' => $student4->iduser]);
+
+        // Actualizar la tabla siscode con iduser
+        DB::table('siscode')->where('siscode', '202400004')->update([
+            'iduser' => $student4->iduser,
+        ]);
 
         // Student 5 User------------------------------
+        
+        // Obtener el siscode antes de crear el student
+        $siscode5 = DB::table('siscode')->where('siscode', '202400005')->first();
+
+        // Crear el student
         $student5 = User::create([
             'nameuser' => 'Student 5 User',
             'lastnameuser' => 'Student',
             'emailuser' => 'student5@example.com',
             'passworduser' => bcrypt('password*'),
-            'idsiscode' => 5,
-            'use_iduser' => $teacher->iduser,
+            'idsiscode' => $siscode5->idsiscode,
+            'use_iduser' => $teacher1->iduser,
         ]);
+
+        // Asignar el rol al student
         RoleUser::create([
             'iduser' => $student5->iduser,
             'idrol' => 3,
         ]);
-        // Asigna el código SIS al estudiante
-        $siscode5 = DB::table('siscode')->where('siscode', '202400005')->first();
-        DB::table('siscode')->where('siscode', '202400005')->update(['iduser' => $student5->iduser]);
+
+        // Actualizar la tabla siscode con iduser
+        DB::table('siscode')->where('siscode', '202400005')->update([
+            'iduser' => $student5->iduser,
+        ]);
 
         // Student 6 User------------------------------
+        
+        // Obtener el siscode antes de crear el student
+        $siscode6 = DB::table('siscode')->where('siscode', '202400006')->first();
+
+        // Crear el student
         $student6 = User::create([
             'nameuser' => 'Student 6 User',
             'lastnameuser' => 'Student',
             'emailuser' => 'student6@example.com',
             'passworduser' => bcrypt('password*'),
-            'idsiscode' => 6,
-            'use_iduser' => $teacher->iduser,
+            'idsiscode' => $siscode6->idsiscode,
+            'use_iduser' => $teacher1->iduser,
         ]);
+
+        // Asignar el rol al student
         RoleUser::create([
             'iduser' => $student6->iduser,
             'idrol' => 3,
         ]);
-        // Asigna el código SIS al estudiante
-        $siscode6 = DB::table('siscode')->where('siscode', '202400006')->first();
-        DB::table('siscode')->where('siscode', '202400006')->update(['iduser' => $student6->iduser]);
+
+        // Actualizar la tabla siscode con iduser
+        DB::table('siscode')->where('siscode', '202400006')->update([
+            'iduser' => $student6->iduser,
+        ]);
     }
 }
