77a0edaae067fe4e451c6a08242bf1dd2e76d635
julio <juliocesarsevericheorellana@gmail.com>
2024-11-25 21:34:24 -0400

    Add Siscode registered Send Emails

---
+++ resources/views/emails/sendEmails.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>
+            Prueba de titulo html
+        </title>
+    </head>
+    <body>
+        <h1>{{ $details ['title'] }}</h1>
+        <p>{{ $details ['body'] }}</p>
+    </body>
+</html>
